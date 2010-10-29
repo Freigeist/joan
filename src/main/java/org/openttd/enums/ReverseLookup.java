4fb5dd9ab293295b60f7f15a456999ecc6b24eee
dih <openttd@dihedral.de>
2010-10-29 23:06:56 +0200

    Add: proper reverse lookup handling

---
+++ src/main/java/org/openttd/enums/ReverseLookup.java
@@ -0,0 +1,25 @@
+package org.openttd.enums;
+
+import java.util.HashMap;
+
+
+/**
+ *
+ * @author Nathanael Rebsch
+ */
+public class ReverseLookup<T, E extends Enum<E> & Reverseable<T>>
+{
+    private final HashMap<T, E> map = new HashMap<T, E>();
+
+    public ReverseLookup (final Class<E> c)
+    {
+        for (E constant : c.getEnumConstants()) {
+            map.put(constant.getValue(), constant);
+        }
+    }
+
+    public E get (final T value)
+    {
+        return map.get(value);
+    }
+}
