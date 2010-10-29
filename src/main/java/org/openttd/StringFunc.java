39ca026c5efe7050f92f25ad28a274ef5c9f2a10
dih <openttd@dihedral.de>
2010-10-29 19:48:56 +0200

    Change: remove old debug messages

--- src/main/java/org/openttd/StringFunc.java
+++ src/main/java/org/openttd/StringFunc.java
@@ -16,10 +16,8 @@ public final class StringFunc {
         StringBuilder sb = new StringBuilder(str);
 
         for (int i = 0; i < str.length(); i++) {
-            System.out.printf("Hex: %d %x  Letter: %s\n", i, str.codePointAt(i),str.substring(i, i+1));
             if ((str.codePointAt(i) >= 0xDB80 && str.codePointAt(i) <= 0xDBFF) ||
                 (str.codePointAt(i) >= 0xE000 && str.codePointAt(i) <= 0xF8FF)) {
-                    System.out.println("Deleting: " + i);
                     sb.deleteCharAt(i);
             }
         }
