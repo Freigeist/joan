5e530b28a4965b205b08d7addba1a1d3dd4da1d2
dih <openttd@dihedral.de>
2010-10-30 00:27:04 +0200

    Change: reverse lookup implementation for VehicleType

--- src/main/java/org/openttd/network/VehicleType.java
+++ src/main/java/org/openttd/network/VehicleType.java
@@ -4,16 +4,43 @@
  */
 package org.openttd.network;
 
+import org.openttd.enums.ReverseLookup;
+import org.openttd.enums.Reverseable;
+
 /**
  *
  * @author nathanael
  */
-public enum VehicleType
+public enum VehicleType implements Reverseable<Integer>
 {
-    NETWORK_VEH_TRAIN,
-    NETWORK_VEH_LORRY,
-    NETWORK_VEH_BUS,
-    NETWORK_VEH_PLANE,
-    NETWORK_VEH_SHIP,
-    NETWORK_VEH_END;
+    NETWORK_VEH_TRAIN  (0),
+    NETWORK_VEH_LORRY  (1),
+    NETWORK_VEH_BUS    (2),
+    NETWORK_VEH_PLANE  (3),
+    NETWORK_VEH_SHIP   (4),
+    NETWORK_VEH_END    (5);
+
+    private Integer value;
+    private static final ReverseLookup<Integer, VehicleType> lookup = new ReverseLookup<Integer, VehicleType>(VehicleType.class);
+
+    VehicleType (int i)
+    {
+        value = i;
+    }
+
+    public static boolean isValid (int i)
+    {
+        return valueOf(i) != null;
+    }
+
+    @Override
+    public Integer getValue ()
+    {
+        return value;
+    }
+
+    public static VehicleType valueOf (int i)
+    {
+        return lookup.get(i);
+    }
 }
