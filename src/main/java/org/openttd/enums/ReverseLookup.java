06155f692b93f2240e7076237002499082d8d0c9
dih <openttd@dihedral.de>
2011-01-08 16:26:34 +0100

    Change: Allow ReverseLookup to be more Dynamic if needed.

--- src/main/java/org/openttd/enums/ReverseLookup.java
+++ src/main/java/org/openttd/enums/ReverseLookup.java
@@ -21,13 +21,26 @@ package org.openttd.enums;
 import java.util.HashMap;
 
 /**
- *
+ * Reverse Lookup of enumerations.
  * @author Nathanael Rebsch
  */
 public class ReverseLookup<T, E extends Reversible<T>>
 {
+    /**
+     * The reverse lookup table.
+     * Only used internally.
+     */
     private final HashMap<T, E> map = new HashMap<T, E>();
 
+    /**
+     * Empty constructor used for Dynamic Enums.
+     */
+    public ReverseLookup () {}
+
+    /**
+     * Constructor. Automatically creates a reverse lookup table if E is a subclass of Enum.
+     * @param clazz Class<E extends Enum> must be true for this constructor.
+     */
     public ReverseLookup (final Class<E> clazz)
     {
         for (E constant : clazz.getEnumConstants()) {
@@ -35,6 +48,22 @@ public class ReverseLookup<T, E extends Reversible<T>>
         }
     }
 
+    /**
+     * Dynamically extend the reverse lookup table.
+     * @param e Instance of the enumeration to be included in the reverse lookup table.
+     */
+    public void add (E e)
+    {
+        if (!map.containsValue(e)) {
+            map.put(e.getValue(), e);
+        }
+    }
+
+    /**
+     * Get an instance of the enumeration corresponding to the value given.
+     * @param value Value to which an instance of the enumeration should be returned.
+     * @return Instance of the corresponding enumeration or null if not found.
+     */
     public E get (final T value)
     {
         return map.get(value);
