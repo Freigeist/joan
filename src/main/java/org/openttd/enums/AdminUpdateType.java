2152131597abbcef0aaa8934973753c5bd1dc774
dih <openttd@dihedral.de>
2013-10-04 10:37:22 +0200

    Fix: receiving GAMESCRIPT updates (Zuu)

--- src/main/java/org/openttd/enums/AdminUpdateType.java
+++ src/main/java/org/openttd/enums/AdminUpdateType.java
@@ -33,7 +33,8 @@ public enum AdminUpdateType implements Reversible<Integer>
     ADMIN_UPDATE_CONSOLE         (6),
     ADMIN_UPDATE_CMD_NAMES       (7),
     ADMIN_UPDATE_CMD_LOGGING     (8),
-    ADMIN_UPDATE_END             (9);
+    ADMIN_UPDATE_GAMESCRIPT      (9),
+    ADMIN_UPDATE_END             (10);
 
     private Integer value;
     private static final ReverseLookup<Integer, AdminUpdateType> lookup;
